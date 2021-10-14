e5dc0686bb771adbbe4687d217961aad87a7ce71
tadrossalama <salamatadros@gmail.comm>
2021-10-14 09:05:15 -0700

    add model and app

---
+++ app.py
@@ -0,0 +1,23 @@
+from fastai.learner import export
+import streamlit as st
+import pickle
+from PIL import Image
+from fastai.vision.all import *
+from pathlib import Path
+import coremltools as ct
+
+
+path = Path()
+learn_inf = load_learner(path/'model.pkl')
+
+
+
+st.title('Coral Classifier :mag_right:')
+upload = st.file_uploader('Upload picture of coral here')
+if upload is not None:
+
+    image = PILImage.create(upload)
+    st.image(image, caption='Uploaded Image.')
+    pred,pred_idx,probs = learn_inf.predict(image)
+
+    st.title(f'I am {probs[pred_idx] * 100:.04f}% sure this is a {pred} coral.')
